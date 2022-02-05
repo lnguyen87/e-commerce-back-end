5341a0671c90fc9a51b713c65ca072c697ca9a95
lnguyen87 <luc_nguyen@outlook.com>
2022-02-04 20:12:06 -0700

    updated category routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -93,7 +93,7 @@ router.delete('/:id', (req, res) => {
         res.status(404).json({ message: 'No tag found with this id' });
         return;
       }
-      res.json(dbUserData);
+      res.json(dbTagData);
     })
     .catch((err) => {
       console.log(err);
