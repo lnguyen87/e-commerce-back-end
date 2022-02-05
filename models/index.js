f3574b44f7d85bbd049ae74727886d3b975ee446
lnguyen87 <luc_nguyen@outlook.com>
2022-02-04 20:37:16 -0700

    updated server.js to start mysql server, fixed index.js model to run seed data

--- models/index.js
+++ models/index.js
@@ -17,7 +17,6 @@ Category.hasMany(Product, {
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
-  as: 'product_tag',
   foreignKey: 'product_id',
 });
 
